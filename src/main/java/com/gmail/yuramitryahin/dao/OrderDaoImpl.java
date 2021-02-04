eb755028d1b8e01cafaa65b94cecb22f8c478edf
Yura <yuramitryahin@gmail.com>
2021-02-04 18:21:36 +0200

    fixed bugs

--- src/main/java/com/gmail/yuramitryahin/dao/OrderDaoImpl.java
+++ src/main/java/com/gmail/yuramitryahin/dao/OrderDaoImpl.java
@@ -36,7 +36,7 @@ public class OrderDaoImpl implements OrderDao {
     @Override
     public List<Order> getOrdersHistory(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            return session.createQuery("select o from Order o "
+            return session.createQuery("select distinct o from Order o "
                     + "left join fetch o.tickets t "
                     + "left join fetch t.movieSession ms "
                     + "left join fetch ms.cinemaHall "
